3e03cd89d2c2bf3a58f77c2f72000d16a53ce339
Alex Lanz <alex.lanz@aboutbits.it>
2022-02-09 14:43:53 +0100

    env file management

---
+++ env-file/main.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+env_keys = list(dict(os.environ).keys())
+
+prefix = str(os.environ.get("INPUT_ENVIRONMENT_VARIABLE_PREFIX", "X_"))
+output = ""
+
+for key in env_keys:
+    if key.startswith(prefix):
+        output += key.split(prefix)[1] + "=" + os.environ.get(key) + "\n"
+
+directory = os.environ.get("INPUT_WORKING_DIRECTORY", ".")
+source_file = os.environ.get("INPUT_SOURCE_FILE")
+destination_file = os.environ.get("INPUT_DESTINATION_FILE", ".env")
+
+directory_str = str(directory)
+source_file_str = str(source_file)
+destination_file_str = str(destination_file)
+
+if source_file is not None:
+    shutil.copyfile(os.path.join(directory_str, source_file_str), os.path.join(directory_str, destination_file_str))
+
+with open(os.path.join(directory_str, destination_file_str), "a") as text_file:
+    text_file.write(output)
