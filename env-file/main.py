dd93e49c3db783ce1bde45251ffc6718d572b8ff
Alex Lanz <alex.lanz@aboutbits.it>
2022-03-02 13:50:24 +0100

    Attempt to fix env-file action

--- env-file/main.py
+++ env-file/main.py
@@ -3,16 +3,16 @@ import shutil
 
 env_keys = list(dict(os.environ).keys())
 
-prefix = str(os.environ.get("INPUT_ENVIRONMENT_VARIABLE_PREFIX", "X_"))
+prefix = str(os.environ.get("INPUT_ENVIRONMENT-VARIABLE-PREFIX", "X_"))
 output = ""
 
 for key in env_keys:
     if key.startswith(prefix):
         output += key.split(prefix, 1)[1] + "=" + os.environ.get(key) + "\n"
 
-directory = os.environ.get("INPUT_WORKING_DIRECTORY", ".")
-source_file = os.environ.get("INPUT_SOURCE_FILE")
-destination_file = os.environ.get("INPUT_DESTINATION_FILE", ".env")
+directory = os.environ.get("INPUT_WORKING-DIRECTORY", ".")
+source_file = os.environ.get("INPUT_SOURCE-FILE")
+destination_file = os.environ.get("INPUT_DESTINATION-FILE", ".env")
 
 directory_str = str(directory)
 source_file_str = str(source_file)
