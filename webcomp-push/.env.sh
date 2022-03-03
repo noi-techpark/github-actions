e2a8df2394be828fe498060a22ac246327bed7e5
Simon Dalvai <s.dalvai@noi.bz.it>
2022-03-03 09:03:20 +0100

    webcomp-push: try export instead of .env

---
+++ webcomp-push/.env.sh
@@ -0,0 +1,7 @@
+export API_URL_DEV=https://api.webcomponents.opendatahub.testingmachine.eu/
+export API_URL_PROD=https://api.webcomponents.opendatahub.bz.it/
+
+export KEYCLOAK_URL=https://auth.opendatahub.testingmachine.eu/auth/
+export KEYCLOAK_REALM=noi
+export KEYCLOAK_CLIENT_ID=it.bz.opendatahub.webcomponents.api
+export KEYCLOAK_CLIENT_SECRET=
--- webcomp-push/entrypoint.sh
+++ webcomp-push/entrypoint.sh
@@ -3,4 +3,6 @@ set -euxo pipefail
 
 echo -n $1 >> .env
 
+sh .env.sh
+
 python3 /webcompstore-cli.py --post $2
