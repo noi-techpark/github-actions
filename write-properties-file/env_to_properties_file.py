868ea8f9672a5d1b2bc9926a2d7754383f424285
Peter Moser <p.moser@noi.bz.it>
2022-06-23 09:47:20 +0200

    write-properties-file: fix wrong argv parameter / 4

--- write-properties-file/env_to_properties_file.py
+++ write-properties-file/env_to_properties_file.py
@@ -6,8 +6,8 @@ from configobj import ConfigObj
 
 env_keys = list(dict(os.environ).keys())
 
-prefix = str(os.environ.get("INPUT_PROPERTIESFILE-ENVIRONMENT-VARIABLE-PREFIX", "PROPS_"))
-path = sys.argv[1]
+prefix = sys.argv[1]
+path = sys.argv[2]
 
 config = ConfigObj(path)
 
@@ -17,5 +17,4 @@ for key in env_keys:
     if key.startswith(prefix):
        config[f"{key.split(prefix, 1)[1]}"] = os.environ.get(key)
 
-print(config)
 config.write()
