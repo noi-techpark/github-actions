0f59387ebe8344c672e9e7d0ae9ce527ed1165a3
henri <henri@moosbauer.com>
2023-06-20 16:44:38 +0200

    add test for origin

--- webcomp-test/index.js
+++ webcomp-test/index.js
@@ -3,19 +3,35 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import core from "@actions/core";
-import github from "@actions/github";
-import io from "@actions/io";
 import fs from "fs";
+
+function originTest(path, keyword) {
+    let entries = fs.readdirSync(path);
+
+    return entries.map((entry) => {
+        if (fs.lstatSync(entry).isDirectory) {
+            return originTest(entry, keyword);
+        } else {
+            return fs.readFileSync(entry, "utf-8").includes("origin");
+        }
+    }).reduce((prev, current) => { return prev ||current }, false);
+}
 
 try {
     const originTestEnabled = core.getBooleanInput("origin-test-enabled");
-    console.log(`Testing for origin is${originTestEnabled ? " " : " not "}enabled`);
+    const originTestKeyword = core.getInput("origin-test-keyword");
+
+    if (originTestEnabled) {
+        console.log(`Testing for origin is enabled with keyword ${originTestKeyword}`);
+    } else {
+        console.log(`Testing for origin is not enabled`);
+    }
 
     const workingDirectory = core.getInput("working-directory");
-    console.log(workingDirectory);
 
-    let entries = fs.readdirSync(workingDirectory);
-    console.log(entries);
+    if (originTestEnabled) {
+        if (!originTest(workingDirectory, originTestKeyword)) core.setFailed("No occurrence of origin found");
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
