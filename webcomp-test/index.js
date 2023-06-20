a60a2b6041cf2fd4871bd81f12536727b7180fcd
henri <henri@moosbauer.com>
2023-06-20 16:01:04 +0200

    add log

--- webcomp-test/index.js
+++ webcomp-test/index.js
@@ -2,14 +2,18 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-const core = require("@actions/core");
-const fs = require("fs");
+import core from "@actions/core";
+import github from "@actions/github";
+import io from "@actions/io";
+import fs from "fs";
 
 try {
     const originTestEnabled = core.getInput("originTestEnabled");
     console.log(`Testing for origin is${originTestEnabled ? " " : " not "}enabled`);
 
     const workingDirectory = core.getInput("workingDirectory");
+
+    console.log(workingDirectory);
 
     let entries = fs.readdirSync(workingDirectory);
 
