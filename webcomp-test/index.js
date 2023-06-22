5d0c99a69243aa93e4271ade7ef6cef6ae629be2
henri <henri@moosbauer.com>
2023-06-22 09:47:15 +0200

    add file ext skipping

--- webcomp-test/index.js
+++ webcomp-test/index.js
@@ -5,37 +5,51 @@
 import core from "@actions/core";
 import fs from "fs";
 
-function originTest(path, keyword) {
-    console.log(`Reading entries of path: ${path}`);
+function originTest(path, keyword, fileExt) {
     let entries = fs.readdirSync(path);
 
     return entries.map((entry) => {
         const entryPath = `${path}/${entry}`;
 
         if (fs.lstatSync(entryPath).isDirectory()) {
-            return originTest(entryPath, keyword);
+            return originTest(entryPath, keyword, fileExt);
         } else {
-            console.log(`Checking for occurence of ${keyword} in ${entryPath}`);
+            const hasValidExt = [...fileExt].map((ext) => entryPath.endsWith(ext)).reduce((prev, current) => prev || current, false)
+            if (!hasValidExt) {
+                console.log(`Skipping ${entryPath}`);
+                return false;
+            }
+
+            console.log(`Checking ${entryPath}`);
             let includesKeyword = fs.readFileSync(entryPath, "utf-8").includes(keyword);
-            if (includesKeyword) console.log(`Found ${keyword} in ${entryPath}`);
+
+            if (includesKeyword)
+                console.log(`Found ${keyword} in ${entryPath}`);
+
             return includesKeyword
         }
-    }).reduce((prev, current) => { return prev ||current }, false);
+    }).reduce((prev, current) => prev || current, false);
 }
 
 try {
     const originTestEnabled = core.getBooleanInput("origin-test-enabled");
     const originTestDirectory = core.getInput("origin-test-directory");
     const originTestKeyword = core.getInput("origin-test-keyword");
+    const originTestFileExt = core.getInput("origin-test-file-ext").split(" ");
 
     if (originTestEnabled) {
         console.log(`Testing for origin is enabled in directory ${originTestDirectory} with keyword ${originTestKeyword}`);
+        console.log(`Searching in files with extensions: ${originTestFileExt}`);
     } else {
         console.log(`Testing for origin is not enabled`);
     }
 
     if (originTestEnabled) {
-        if (!originTest(originTestDirectory, originTestKeyword)) core.setFailed("No occurrence of origin found");
+        if (!originTest(originTestDirectory, originTestKeyword, originTestFileExt)) {
+            const msg = "No occurrence of origin found";
+            core.error(msg);
+            core.setFailed(msg);
+        }
     }
 } catch (error) {
     core.setFailed(error.message);
