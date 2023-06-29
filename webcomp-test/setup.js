2e1b452b0af94077c74bfdfa9c689db6834a9668
henri <henri@moosbauer.com>
2023-06-29 14:08:01 +0200

    use node16 and https module instead of fetch api

--- webcomp-test/setup.js
+++ webcomp-test/setup.js
@@ -1,15 +1,43 @@
 import { exec } from "child_process";
 import fs from "fs";
+import https from "https";
 
-console.log("Downloading validator.js");
-const validatorRes = await fetch("https://webcomponents.opendatahub.com/validator/validator.js");
-const validatorStr = await validatorRes.text();
-fs.writeFileSync("validator/validator.js", validatorStr);
+const validatorUrl = "https://webcomponents.opendatahub.com/validator/validator.js";
+const schemaUrl = "https://webcomponents.opendatahub.com/schemas/wcs-manifest-schema.json";
 
-console.log("Downloading wcs-manifest-schema.json");
-const schemaRes = await fetch("https://webcomponents.opendatahub.com/schemas/wcs-manifest-schema.json");
-const schemaStr = await schemaRes.text();
-fs.writeFileSync("schemas/wcs-manifest-schema.json", schemaStr);
+https.get(validatorUrl, (res) => {
+  let data = '';
+  res.on('data', chunk => {
+    data += chunk;
+  });
+  res.on('end', () => {
+    fs.writeFileSync("validator/validator.js", data);
+  })
+}).on('error', err => {
+  console.log(err.message);
+})
+
+https.get(schemaUrl, (res) => {
+  let data = '';
+  res.on('data', chunk => {
+    data += chunk;
+  });
+  res.on('end', () => {
+    fs.writeFileSync("schemas/wcs-manifest-schema.json", schemaStr);
+  })
+}).on('error', err => {
+  console.log(err.message);
+})
+
+// console.log("Downloading validator.js");
+// const validatorRes = await fetch("https://webcomponents.opendatahub.com/validator/validator.js");
+// const validatorStr = await validatorRes.text();
+// fs.writeFileSync("validator/validator.js", validatorStr);
+
+// console.log("Downloading wcs-manifest-schema.json");
+// const schemaRes = await fetch("https://webcomponents.opendatahub.com/schemas/wcs-manifest-schema.json");
+// const schemaStr = await schemaRes.text();
+// fs.writeFileSync("schemas/wcs-manifest-schema.json", schemaStr);
 
 console.log("Installing node modules...");
 exec("npm ci", (error, stdout, stderr) => {
