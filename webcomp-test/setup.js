94d53d43601be52ce5785a2713c79d67333fc034
henri <henri@moosbauer.com>
2023-06-29 14:15:01 +0200

    add logging

--- webcomp-test/setup.js
+++ webcomp-test/setup.js
@@ -54,6 +54,18 @@ exec("npm ci", (error, stdout, stderr) => {
     console.log(`stdout: ${stdout}`);
 });
 
+exec("pwd", (error, stdout, stderr) => {
+  if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+  }
+  if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
+
 exec("ls -la", (error, stdout, stderr) => {
   if (error) {
       console.log(`error: ${error.message}`);
